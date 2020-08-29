0bf3eefabde31ba078947e242281ebc56121c73b
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-29 01:30:42 -0400

    just kidding now im done

--- src/main/java/com/revature/repositories/ReimbursementsRepository.java
+++ src/main/java/com/revature/repositories/ReimbursementsRepository.java
@@ -5,6 +5,15 @@ public class ReimbursementsRepository {
     private String baseInsert = "INSERT INTO project_1.ers_reimbursements ";
     private String baseUpdate = "UPDATE project_1.ers_reimbursements ";
 
-
+    public void addFile(){
+        File file = new File("myimage.gif");
+        FileInputStream fis = new FileInputStream(file);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO images VALUES (?, ?)");
+        ps.setString(1, file.getName());
+        ps.setBinaryStream(2, fis, file.length());
+        ps.executeUpdate();
+        ps.close();
+        fis.close();
+    }
 
 }
